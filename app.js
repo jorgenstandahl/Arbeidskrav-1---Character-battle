23e985b6d00ce0df712f31f88e8ac4e7f232ca33
jorgenstandahl <jorg1pettersen@gmail.com>
2025-02-09 11:05:19 +0100

    Added node modules and json file. Added function; playerinput gets stores in local storage

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
 //Her kommer din Javascript-kode. Kommentarene er lagt til for å hjelpe deg med å dele opp oppgavene,
 // du kan slette disse hvis du ønsker.
 
+// const { setGlobalDispatcher } = require("undici-types")
+
 // Del 1: Lag karakter og lagre karakteren i localStorage
+const nameInput = document.getElementById("character-name")
+const hpInput = document.getElementById("character-hp")
+const attackInput = document.getElementById("attack-damage")
+const characterImageChoice = document.getElementById("Bilde 1")
+console.log(nameInput.value);
+
+
+
+const makeCharacter =() => {
+const playerCharacter = {
+    name: nameInput.value,
+    hp: hpInput.value,
+    attack: attackInput.value
+}
+localStorage.setItem('playerCharacter',JSON.stringify(playerCharacter))
+}
+
+
+
 
 //Seksjon 2: Generer fiende
 
