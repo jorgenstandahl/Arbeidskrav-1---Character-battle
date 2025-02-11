0d7db32c824efe3328d64b521d20a6a15aeaf589
jorgenstandahl <jorg1pettersen@gmail.com>
2025-02-11 16:15:20 +0100

    Lagt til DOM manipulering - karakter og fiende vises i sloss sonen så fort de er skapt

--- app.js
+++ app.js
@@ -9,78 +9,172 @@ const hpInput = document.getElementById("character-hp");
 const attackInput = document.getElementById("attack-damage");
 const characterImageChoice = document.getElementsByClassName("profile-pics");
 const createButton = document.getElementById("create-character");
-let chosenImg = ''
+let chosenImg = "";
 console.log(nameInput.value);
 
 document.querySelectorAll("img").forEach(function (img) {
-    img.onclick = function () {
-      chosenImg = this.alt;
-    };
-  });
+  img.onclick = function () {
+    chosenImg = this.src;
+  };
+});
 
 const makeCharacter = () => {
   const playerCharacter = {
     name: nameInput.value,
     hp: hpInput.value,
     attack: attackInput.value,
-    profileImg: chosenImg
+    profileImg: chosenImg,
   };
   localStorage.setItem("playerCharacter", JSON.stringify(playerCharacter));
+  showCharacter()
 };
 
-
+const showCharacter = () => {
+    let collectedPlayer = localStorage.getItem("playerCharacter");
+    collectedPlayer = JSON.parse(collectedPlayer);
+    const battleArea = document.getElementById("battle-area");
+  
+    const displayExist = document.getElementById("character-display")
+    if (displayExist) {
+      displayExist.remove()
+    }
+  
+    let characterDisplay = document.createElement("div");
+    characterDisplay.id = "character-display";
+    characterDisplay.classList = "profile-card";
+  
+    battleArea.appendChild(characterDisplay);
+  
+    let displayH2 = document.createElement("h2");
+    displayH2.innerText = "Helten";
+  
+    let displayImg = document.createElement("img");
+    displayImg.id = "char-img";
+    displayImg.alt = "Profilbilde";
+    displayImg.src = collectedPlayer.profileImg;
+  
+    let charNameP = document.createElement("p");
+    charNameP.id = "char-name";
+    charNameP.innerText = collectedPlayer.name;
+  
+    let charHpP = document.createElement("p");
+    charHpP.id = "char-hp";
+    charHpP.innerText = "Hp: " + collectedPlayer.hp;
+  
+    let charAttackP = document.createElement("p");
+    charAttackP = "char-attack";
+    charAttackP = "Attack: " + collectedPlayer.attack;
+  
+    characterDisplay.append(
+      displayH2,
+      displayImg,
+      charNameP,
+      charHpP,
+      charAttackP
+    );
+  };
 
 createButton.addEventListener("click", makeCharacter);
 
 //Seksjon 2: Generer fiende
-const generateEnemy = document.getElementById('generate-enemy')
+const generateEnemy = document.getElementById("generate-enemy");
 
+const randomizer = (maxValue) => {
+  return Math.floor(Math.random() * maxValue);
+};
+const enemyName = () => {
+  if (randomizer(3) === 0) {
+    return "Goblin";
+  } else if (randomizer(3) === 1) {
+    return "Ork";
+  } else {
+    return "Drage";
+  }
+};
 
-const randomizer = (maxValue) =>{
-    return Math.floor(Math.random()* maxValue)
-}
-const enemyName = () =>{
-    if (randomizer(3) === 0){
-        return 'Goblin'
-    }
-    else if (randomizer(3)=== 1){
-        return 'Ork'
-    }
-    else {return 'Drage'}
-}
-
-const enemyImg = () =>{
-    if (randomizer(3) === 0){
-        return "./assets/dragon.jpg"
-    }
-    else if (randomizer(3)=== 1){
-        return "./assets/monster.jpg"
-    }
-    else {return "./assets/swamp-monster.jpg"}
-}
+const enemyImg = () => {
+  if (randomizer(3) === 0) {
+    return "./assets/dragon.jpg";
+  } else if (randomizer(3) === 1) {
+    return "./assets/monster.jpg";
+  } else {
+    return "./assets/swamp-monster.jpg";
+  }
+};
 console.log(enemyImg());
 
-
-const makeEnemy = () =>{
-const enemyCharacter = {
+const makeEnemy = () => {
+  const enemyCharacter = {
     name: enemyName(),
-    enemyPic:enemyImg(),
-    enemyHp: randomizer(101)+50,
-    enemyAttack: randomizer(31)+10
-}
-document.getElementById("enemy-name").innerHTML = "Name: " + enemyCharacter.name
-document.getElementById("enemy-img").src = enemyCharacter.enemyPic
-document.getElementById("enemy-hp").innerHTML = "HP: "+ enemyCharacter.enemyHp
-document.getElementById("enemy-attack").innerHTML = "Attack: "+ enemyCharacter.enemyAttack
- 
-localStorage.setItem("enemyCharacter", JSON.stringify(enemyCharacter))
-}
-generateEnemy.addEventListener("click",makeEnemy)
+    enemyPic: enemyImg(),
+    enemyHp: randomizer(101) + 50,
+    enemyAttack: randomizer(31) + 10,
+  };
+  
+  localStorage.setItem("enemyCharacter", JSON.stringify(enemyCharacter));
+  showEnemy()
+};
+
+const showEnemy = () => {
+    let collectedEnemy = localStorage.getItem("enemyCharacter");
+    collectedEnemy = JSON.parse(collectedEnemy);
+    const battleArea = document.getElementById("battle-area");
+  
+    const displayExist = document.getElementById("enemy-fight-display")
+    if (displayExist) {
+      displayExist.remove()
+    }
+  
+    let enemyDisplay = document.createElement("div");
+    enemyDisplay.id = "enemy-fight-display";
+    enemyDisplay.classList = "profile-card";
+  
+    battleArea.appendChild(enemyDisplay);
+  
+    let displayH2 = document.createElement("h2");
+    displayH2.innerText = "Fiende";
+  
+    let displayImg = document.createElement("img");
+    displayImg.id = "enemy-fight-img";
+    displayImg.alt = "Fiendens profilbilde";
+    displayImg.src = collectedEnemy.enemyPic;
+  
+    let enemyNameP = document.createElement("p");
+    enemyNameP.id = "enemy-fight-name";
+    enemyNameP.innerText = collectedEnemy.name;
+  
+    let enemyHpP = document.createElement("p");
+    enemyHpP.id = "enemy-fight-hp";
+    enemyHpP.innerText = "Hp: " + collectedEnemy.enemyHp;
+  
+    let enemyAttackP = document.createElement("p");
+    enemyAttackP = "enemy-fight-attack";
+    enemyAttackP = "Attack: " + collectedEnemy.enemyAttack;
+  
+    enemyDisplay.append(
+      displayH2,
+      displayImg,
+      enemyNameP,
+      enemyHpP,
+      enemyAttackP
+    );
+  };
+
+generateEnemy.addEventListener("click", makeEnemy);
 
 // Seksjon 3: Sloss!
 
-//Legge inn heltene fra localStorage og vise dem i HTML
+const fetchHero = () => {
+  let collectedHero = localStorage.getItem("playerCharacter");
+  collectedHero = JSON.parse(collectedHero);
+  return collectedHero;
+};
 
+
+
+
+
+//Legge inn heltene fra localStorage og vise dem i HTML
 
 //Du skal vise frem helten og fienden. Se HTML-dokumentet for hvordan fremvisningen skal se ut, med tanke på hvilke tagger, hierarki og hvilke klasser de skal ha.
 //Du skal lage den strukturen som vist i HTML, her i Javascript og legge de til i div'en "battle-arena" fra HTML.
